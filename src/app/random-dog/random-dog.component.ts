abbdde1f0ae5fe371ec6de2dff1857ec605323ad
John Lace <lace.john@gmail.com>
2018-10-29 14:14:24 +0000

    Removed random image caching.

--- src/app/random-dog/random-dog.component.ts
+++ src/app/random-dog/random-dog.component.ts
@@ -9,39 +9,19 @@ import { DogApiService } from '../dog-api.service';
 export class RandomDogComponent implements OnInit {
 
   public imageUrl: string;
-  public images: Array<string> = new Array<string>();
-  public selectedIndex: number = -1;
 
   constructor(private _service: DogApiService) {
-    this.next();
+    this.randomise();
   }
 
   ngOnInit() {
     
   }
 
-  private showImageAtIndex() {
-    this.imageUrl = this.images[this.selectedIndex];
+  public randomise() {
+    this._service.getRandomImage()
+      .subscribe(imageUrl => {
+        this.imageUrl = imageUrl;
+      });
   }
-
-  public next() {
-    if (this.selectedIndex === this.images.length -1) {
-      this._service.getRandomImage()
-        .subscribe(imageUrl => {
-          this.images.push(imageUrl);
-          this.selectedIndex++;
-          this.showImageAtIndex();
-        })
-    }
-    else {
-      this.selectedIndex++;
-      this.showImageAtIndex();
-    }
-  }
-
-  public previous() {
-    this.selectedIndex--;
-    this.showImageAtIndex();
-  }
-
 }
